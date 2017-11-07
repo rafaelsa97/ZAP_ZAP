545aac8c1ae202eee01b75c94fae20215fcc0b5a
Rafael Santos de Almeida <rafaelsa97@gmail.com>
2017-11-06 22:34:15 -0200

    Versão 3 do servidor

--- mtd_svr.py
+++ mtd_svr.py
@@ -5,3 +5,61 @@ def erro(s,idf_r,idf_d):
 
 def ok(s,idf_r,idf_d):
 	s.send(struct.pack('!4H',1,idf_r,idf_d,69))
+	print "ok"
+
+def clist(sock,server_id,id_remet,list):
+	id = 7
+	num = 0
+	print "Chegou aqui"
+	new_list = ""
+	for i in list:
+		if i != 0:
+			id_sock = buscaID(list,i)
+			print "ID SOCKET: " + str(id_sock)
+			num = num + 1
+			new_list = new_list + struct.pack('!H',id_sock)
+	print "Passou"
+	aux = struct.pack("!5H",id,server_id,id_remet,0,num)
+	sock.send(aux + new_list)
+
+	
+def send_message(id_remet,id_dest,data,lista_socket):
+	if id_dest == 0:
+		for i in lista_socket:
+			i.send(data)
+			ok(remet,65535,id_remet)
+			return True
+		
+	else:
+		dest =  procuraSock(id_dest,lista_socket)
+		print "Socket destino: " + str(dest)
+		if dest:
+			dest.send(data)
+			ok(remet,65535,id_remet)
+			return True
+		else:
+			return False
+
+
+def procuraSock(dest,list):
+        if dest != 65535:
+                try:
+                        client = list[dest]
+                        return client
+                except:
+                        return False
+        else:
+                return client
+
+def buscaID(list,sock): 
+    cont = 0
+    while cont < len(list):
+        if list[cont] == sock :
+                return cont
+        else:
+                cont +=1
+
+def fecharSocket(sock,list):
+    ID = buscaID(sock)
+    list[ID] = 0
+    sock.close()
--- servidor.py
+++ servidor.py
@@ -4,43 +4,103 @@
 import socket
 import struct
 import mtd_svr
+import select
+import sys
 
 #Endereco IP e porto da comunicacao
-IP = '127.0.0.1'
-PORTO = 51515
+IP = ''
+PORTO = 51513
+
+SOCKET_LIST = []
+RECV_BUFFER = 4096
+LISTA_ID = [] 
+server_id = 65535
 
 #Criacao do socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #Aportando no porto do IP indicado
 serversocket.bind((IP, PORTO))
 #Esperando por conexao
-serversocket.listen(1)
+serversocket.listen(65534)
+#Colocar na lista de sockets, o socket do servidor
+SOCKET_LIST.append(serversocket)
+
+print "Chat server iniciado no porto: " + str(PORTO)
 
 # Envia identificador para o cliente
 while 1:
-	#Aceita a conexao
-	(client, addr) = serversocket.accept()
-	# Recebe requisicao do cliente
-	data = client.recv(8)
-	if not data:
-		break
-	else:
-		data = struct.unpack('!4H',data)
-		client.send(struct.pack('!4H',1,8,1,8))
 
-	# Recebe mensagens do cliente
-	while 1:
-		header = client.recv(8)
-		header_unpacked = struct.unpack("!4H",header)
-		if(header_unpacked[0] == 4):
-			print "Falou cuzao"
-			break
-		mensagem = ""
-		for i in range(header_unpacked[3]):
-			a = struct.unpack('!B',client.recv(1))
-			mensagem = mensagem + str(chr(a[0]))
-		print mensagem
+    ready_to_read,ready_to_write,in_error = select.select(SOCKET_LIST,[],[],0)
 
-		mtd_svr.ok(client,header_unpacked[1],header_unpacked[2])
-	client.send(struct.pack('!4H',1,8,1,8))
-	client.close
+    for sock in ready_to_read:
+
+                if sock == serversocket:
+                        client,addr = serversocket.accept()
+                        SOCKET_LIST.append(client)
+                               # Recebe requisicao do cliente
+                        print "Cliente (%s, %s) connectado" % addr
+                        data = client.recv(8)
+                        if not data:
+                                break
+                        else:
+                                data = struct.unpack('!4H',data)
+                                if data[0] == 3:
+                                        new_client_id = mtd_svr.buscaID(SOCKET_LIST,client)
+                                        if not new_client_id:
+                                                break
+                                        else:
+                                                mtd_svr.ok(client,server_id,new_client_id)
+
+                else:
+                          # process data recieved from client, 
+                    try:
+                        # receiving data from the socket.                    
+                        data = sock.recv(8)
+                        if data:
+                            msg = ''
+                            tipo,id_remet,id_dest,tam = struct.unpack("!4H",data)
+                            for i in range(tam):
+                                byte = struct.unpack('!B',sock.recv(1))
+                                msg = msg + str(chr(byte[0]))
+                            print tipo,id_remet,id_dest,tam,msg
+
+                            #Mensagem de CREQ
+                            if tipo == 6:
+                                mtd_svr.clist(sock,server_id,id_remet,SOCKET_LIST)
+                                tipo,id_remet,id_dest,nothing = struct.unpack('!4H',sock.recv(8))
+                                print tipo,id_remet,id_dest,nothing
+                                if tipo == 2:
+                                    print "ERROR 404"
+
+                            #Mensagem para outro
+
+                            if tipo == 5:
+                                mtd_svr.ok(sock,server_id,id_remet)
+                                if id_dest == 0:
+                                    for i in SOCKET_LIST:
+                                        i.send(data)
+                                        mtd_svr.ok(sock,65535,id_remet)
+                                        print "JKJAKLSDJAKLSFS"
+                                        error = False
+        
+                                else:
+                                    dest =  procuraSock(id_dest,lista_socket)
+                                    print "Socket destino: " + str(dest)
+                                    if dest:
+                                        dest.send(data)
+                                        ok(remet,65535,id_remet)
+                                        error = False
+                                print error
+
+                            #Mensagem de FLW
+                            if tipo == 4:
+                                if sock in SOCKET_LIST:
+                                    mtd_svr.ok(sock,server_id,id_remet)
+                                    mtd_svr.fecharSocket(sock,SOCKET_LIST)
+                                    
+
+
+                    except:
+                        continue
+
+serversocket.close()
