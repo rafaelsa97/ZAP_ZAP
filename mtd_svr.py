6d9c05b813497c519b769415c246d4f9e1c0c3d7
Rafael <rafaelsa97@ufmg.br>
2017-11-12 14:38:20 -0200

    Programa do servidor atualizado (feito por Bhryan)

--- mtd_svr.py
+++ mtd_svr.py
@@ -1,87 +1,98 @@
 # -*- coding: utf-8 -*-
 import struct
 import socket
+import time
 
-def erro(s,idf_r,idf_d):
-	s.send(struct.pack('!4H',2,idf_r,idf_d,69))
+class bcolors:
+    HEADER = '\033[95m'  # Rosa claro
+    OKBLUE = '\033[94m'  # Azul
+    OKGREEN = '\033[92m' # Verde
+    WARNING = '\033[93m' # Amarelo
+    FAIL = '\033[91m'    # Vermelho
+    ENDC = '\033[0m'     # Branco
+
+def apresentacao():
+    print bcolors.OKBLUE + "\n\n███████╗ █████╗ ██████╗     ███████╗ █████╗ ██████╗ " + bcolors.ENDC
+    print bcolors.OKBLUE + "╚══███╔╝██╔══██╗██╔══██╗    ╚══███╔╝██╔══██╗██╔══██╗" + bcolors.ENDC
+    print bcolors.OKBLUE + "  ███╔╝ ███████║██████╔╝      ███╔╝ ███████║██████╔╝" + bcolors.ENDC
+    print bcolors.OKBLUE + " ███╔╝  ██╔══██║██╔═══╝      ███╔╝  ██╔══██║██╔═══╝ " + bcolors.ENDC
+    print bcolors.OKBLUE + "███████╗██║  ██║██║         ███████╗██║  ██║██║     " + bcolors.ENDC
+    print bcolors.OKBLUE + "╚══════╝╚═╝  ╚═╝╚═╝         ╚══════╝╚═╝  ╚═╝╚═╝     \n" + bcolors.ENDC
+    print bcolors.OKBLUE + "Por Bhryan Henderson e Rafael Santos de Almeida\n" + bcolors.ENDC
+def erro(s,idf_r,idf_d,ordem):
+	s.send(struct.pack('!4H',2,idf_r,idf_d,ordem))
 
 # recebe_cabecalho(cabeçalho empacotado, socket do cliente)
 # Recebe mensagem do cliente
 # Saida: dados do pacote desempacotados
 def recebe_mensagem(data,sock):
 	msg = ''
-	tipo,id_remet,id_dest,ordem,tam = struct.unpack("!5H",data)
-	# Obtém o payload da mensagem
-	for i in range(tam):
-		byte = struct.unpack('!B',sock.recv(1))
-		msg = msg + str(chr(byte[0]))
-	print tipo,id_remet,id_dest,ordem,tam,msg
-	return tipo,id_remet,id_dest,ordem,tam,msg
+	tipo,id_remet,id_dest,ordem = struct.unpack("!4H",data)
+	if tipo == 5:
+		tam = struct.unpack("!H",sock.recv(2))
+		# Obtém o payload da mensagem
+		for i in range(tam[0]):
+			byte = struct.unpack('!B',sock.recv(1))
+			msg = msg + str(chr(byte[0]))
+		return tipo,id_remet,id_dest,ordem,tam[0],msg
+	else:
+		return tipo,id_remet,id_dest,ordem,0,0
 
-def ok(s,idf_r,idf_d):
-	s.send(struct.pack('!4H',1,idf_r,idf_d,69))
-	print "ok enviado"
+def ok(s,idf_r,idf_d,ordem):
+	s.send(struct.pack('!4H',1,idf_r,idf_d,ordem))
 
-def clist(sock,server_id,id_remet,list):
+def clist(sock,server_id,id_remet,list,serversocket):
 	id = 7
 	num = 0
 	new_list = ""
 	for i in list:
-		if i != 0:
+		if i != 0 and i != serversocket and i != sock:
 			id_sock = buscaID(list,i)
-			print "ID SOCKET: " + str(id_sock)
 			num = num + 1
 			new_list = new_list + struct.pack('!H',id_sock)
 	aux = struct.pack("!5H",id,server_id,id_remet,0,num)
 	sock.send(aux + new_list)
+	ok = sock.recv(8)
 
 def buscaID(list,sock):
-    cont = 0
+    cont = 1
     while cont < len(list):
         if list[cont] == sock :
                 return cont
         else:
-                cont +=1
+                cont = cont +1
 
-def send_message(id_remet,id_dest,data,lista_socket):
-	if id_dest == 0:
-		for i in lista_socket:
-			i.send(data)
-			ok(remet,65535,id_remet)
-			return True
-
-	else:
-		dest =  procuraSock(id_dest,lista_socket)
-		print "Socket destino: " + str(dest)
-		if dest:
-			dest.send(data)
-			ok(remet,65535,id_remet)
-			return True
-		else:
-			return False
-
+def encaminha_msg(dest,pacote,list):
+	dest.send(pacote)
+	try:
+		dest.settimeout(5.0)
+		ok = dest.recv(8)
+	except socket.timeout:
+		fecharSocket(dest,list)
 
 def procuraSock(dest,list):
-        if dest != 65535:
-                try:
-                        client = list[dest]
-                        return client
-                except:
-                        return False
+        if dest != 65535 and dest < len(list):
+        	client = list[dest]
+        	if client != 0:
+        		return client
+        	else:
+        		return False
         else:
-                return client
+        	return False
 
 def fecharSocket(sock,list):
-    ID = buscaID(sock)
-    list[ID] = 0
-    sock.close()
+	id = buscaID(list,sock)
+	sock.close()
+	list[id] = 0
 
-def encaminha_msg(list,data,s_aux,client,serversoket):
-	for j in list:
-		if j != serversoket:
-			pacote = data + s_aux
-			print pacote
-			j.send(pacote)
-			print "MANDOU NO BROADCAST"
-			ok = j.recv(8)
-			print "JKJAKLSDJAKLSFS"
+	print "Cliente (%s) fechado" % str(id)
+
+
+def encaminha_msg_broadcast (list,msg, sock,server_socket):
+    for socket in list:#para todos os sockets na lista de sockets observados pelo select
+        if socket != server_socket and socket != sock and socket != 0 :#se o socket nao e o servidor nem quem esta fazendo o broadcast
+            try:
+                socket.send(msg) #tenta enviar a msg
+            except :#se algo der errado, socket corrompido
+                if socket in list:#se o socket esta na lista de sockets observados pelo select
+                    fecharSocket(socket,list) #remove da lista
--- servidor.py
+++ servidor.py
@@ -17,86 +17,99 @@ RECV_BUFFER = 4096
 LISTA_ID = []
 server_id = 65535
 
-#Criacao do socket
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#Aportando no porto do IP indicado
-serversocket.bind((IP, PORTO))
-#Esperando por conexao
-serversocket.listen(65534)
-#Colocar na lista de sockets, o socket do servidor
-SOCKET_LIST.append(serversocket)
 
-print "Chat server iniciado no porto: " + str(PORTO)
+def server():
+    n = 0
+    mtd_svr.apresentacao()
 
-# Envia identificador para o cliente
-while 1:
+    #Criacao do socket
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #Aportando no porto do IP indicado
+    serversocket.bind((IP, PORTO))
+    #Esperando por conexao
+    serversocket.listen(255)
+    #Colocar na lista de sockets, o socket do servidor
+    SOCKET_LIST.insert(n,serversocket)
+    n = n + 1
 
-    ready_to_read,ready_to_write,in_error = select.select(SOCKET_LIST,[],[],0)
+    print "Chat server iniciado no porto: " + str(PORTO)
 
-    for sock in ready_to_read:
+    # Envia identificador para o cliente
+    while 1:
 
-                if sock == serversocket:
-                        client,addr = serversocket.accept()
-                        SOCKET_LIST.append(client)
-                        data = client.recv(8) # Recebe requisicao inicial do cliente para conectar ao servidor
-                        if not data:
-                            print "ERRO\nNão foi possível o cliente conectar ao servidor"
-                            serversocket.close()
-                            sys.exit(0)
-                        else:
-                            print "Cliente (%s, %s) connectado" % addr
-                            data = struct.unpack('!4H',data)
-                            if data[0] == 3: # Identifica se recebeu um pacote tipo OI
-                                    new_client_id = mtd_svr.buscaID(SOCKET_LIST,client)
-                                    if not new_client_id:
-                                            print "Não foi possível atribuir núm.de identificadorao cliente"
-                                            break
-                                    else:
-                                            mtd_svr.ok(client,server_id,new_client_id)
+        ready_to_read,ready_to_write,in_error = select.select(SOCKET_LIST,[],[],0)
 
-                else:
-                          # process data recieved from client,
-                    try:
-                        # receiving data from the socket.
-                        data = sock.recv(10)
+        for sock in ready_to_read:
+
+                    if sock == serversocket:
+                            client,addr = serversocket.accept()
+                            SOCKET_LIST.insert(n,client)
+                            n = n + 1
+                            data = client.recv(8) # Recebe requisicao inicial do cliente para conectar ao servidor
+                            if not data:
+                                print "ERRO\nNão foi possível o cliente conectar ao servidor"
+                                serversocket.close()
+                                sys.exit(0)
+                            else:
+                                print "Cliente (%s, %s) connectado" % addr
+                                data = struct.unpack('!4H',data)
+                                if data[0] == 3: # Identifica se recebeu um pacote tipo OI
+                                        new_client_id = mtd_svr.buscaID(SOCKET_LIST,client)
+                                        if not new_client_id:
+                                                print "Não foi possível atribuir núm.de identificadorao cliente"
+                                                break
+                                        else:
+                                                mtd_svr.ok(client,server_id,new_client_id,0)
+
+                    else:
+                              # process data recieved from client,
+                        try:
+                            # receiving data from the socket.
+                            data = sock.recv(8)
+                        except:
+                            mtd_svr.fecharSocket(sock,SOCKET_LIST)
+                            continue
                         if data:
-                            tipo,id_remet,id_dest,ordem,tam,msg = mtd_svr.recebe_mensagem(data,sock)
+                            if data:
+                                tipo,id_remet,id_dest,ordem,tam,msg = mtd_svr.recebe_mensagem(data,sock)
+                                socketID = mtd_svr.procuraSock(id_remet,SOCKET_LIST)
+                                if socketID != sock:
+                                    print "Socket falso"
+                                    mtd_svr.erro(socket,server_id,id_remet,ordem)
+                                    break    
 
-                            #Mensagem de CREQ
-                            if tipo == 6:
-                                print "entrou no tipo CREQ"
-                                mtd_svr.clist(sock,server_id,id_remet,SOCKET_LIST)
-                                tipo,id_remet,id_dest,nothing = struct.unpack('!4H',sock.recv(8))
-                                print tipo,id_remet,id_dest,nothing
-                                if tipo == 2: # Se tiver erro...
-                                    print "ERROR 404"
+                                #Mensagem de CREQ
+                                if tipo == 6:
+                                    mtd_svr.clist(sock,server_id,id_remet,SOCKET_LIST,serversocket)
 
-                            #Mensagem para outro
-                            if tipo == 5:
-                                print "entrou no tipo MSG"
-                                mtd_svr.ok(sock,server_id,id_remet)
-                                if id_dest == 0: #Broadcast
-                                    # Empacota o payload
+                                #Mensagem para outro
+                                if tipo == 5:
                                     s_aux = ""
+                                    # Empacota o payload
                                     for c in msg:
                                         s_aux = s_aux + struct.pack('!B',ord(c))
-                                    mtd_svr.encaminha_msg(SOCKET_LIST,data,s_aux,client,serversocket) # Encaminha mensagem aos clientes
-                                else: # Mensagem para cliente específico
-                                    dest =  procuraSock(id_dest,lista_socket)
-                                    print "Socket destino: " + str(dest)
-                                    if dest:
-                                        dest.send(data)
-                                        mtd_svr.ok(remet,65535,id_remet)
-                                        error = False
-                                print error
+                                    #Reconstroi o pacote
+                                    tam = struct.pack('!H',tam)
+                                    pacote = data + tam + s_aux
+                                    if id_dest == 0: #Broadcast
+                                        mtd_svr.ok(sock,server_id,id_remet,ordem)                                  
+                                        mtd_svr.encaminha_msg_broadcast(SOCKET_LIST,pacote,sock,serversocket) # Encaminha mensagem aos clientes
+                                    else: # Mensagem para cliente específico
+                                        dest =  mtd_svr.procuraSock(id_dest,SOCKET_LIST)
+                                        if dest != 0:
+                                            mtd_svr.ok(sock,server_id,id_remet,ordem)
+                                            mtd_svr.encaminha_msg(dest,pacote,SOCKET_LIST)
+                                        else:
+                                            mtd_svr.erro(sock,server_id,id_remet,ordem)
 
-                            #Mensagem de FLW
-                            if tipo == 4:
-                                print "entrou no tipo FLW"
-                                if sock in SOCKET_LIST:
-                                    mtd_svr.ok(sock,server_id,id_remet)
-                                    mtd_svr.fecharSocket(sock,SOCKET_LIST)
-                    except:
-                        continue
+                                #Mensagem de FLW
+                                if tipo == 4:
+                                    if sock in SOCKET_LIST:
+                                        mtd_svr.ok(sock,server_id,id_remet,ordem)
+                                        mtd_svr.fecharSocket(sock,SOCKET_LIST)
 
-serversocket.close()
+    serversocket.close()
+
+if __name__ == "__main__": #inicio do programa
+
+    sys.exit(server()) #chama a funcao e sai
